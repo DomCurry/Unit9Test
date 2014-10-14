2022f8efcfd6dd665626fb5a8461ebc554f33bb1
DomCurry <domcurry93@gmail.com>
2014-10-14 20:32:12 +0100

    fixed being able to 'escape' the level
    added death reasons
    added restart button
    added score as height

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,16 +6,61 @@ public class PlayerScript : MonoBehaviour {
 	private VerticalMovement vertScript;
 	private SidewaysMovement sideScript;
 
+	[SerializeField]
+	private GameObject deadText;
+	[SerializeField]
+	private GameObject restartText;
+	[SerializeField]
+	private GameObject spikeReason;
+	[SerializeField]
+	private GameObject bulletReason;
+	[SerializeField]
+	private GameObject turretReason;
+	[SerializeField]
+	private GameObject unknownReason;
+	[SerializeField]
+	private GameObject deathTextHandler;
+
+	[SerializeField]
+	private float textSpeed;
+
+	private bool isDead = false;
+	private Vector3 pos = Vector3.zero;
 	void Start(){
 		vertScript = gameObject.GetComponent<VerticalMovement>();
 		sideScript = gameObject.GetComponent<SidewaysMovement>();
 	}
-
+	void Update(){
+		if(isDead){
+			deathTextHandler.transform.localPosition = new Vector3(Mathf.Lerp(deathTextHandler.transform.localPosition.x, -7f,textSpeed),0f,0f);
+			if(Input.GetKeyDown(KeyCode.Return)){
+				Application.LoadLevel(0);
+			}
+		}
+	}
 	void OnCollisionEnter(Collision collision){
+		DeathMessage (collision.contacts[0].otherCollider.tag);
 		PlayerDeath();
 	}
 	void PlayerDeath(){
 		vertScript.PlayerDeath();
 		sideScript.PlayerDeath();
+		isDead = true;
+	}
+	void DeathMessage(string otherTag){
+		deadText.transform.parent = deathTextHandler.transform;
+		restartText.transform.parent = deathTextHandler.transform;
+		if(otherTag == "Spike" || otherTag == "Base"){
+			spikeReason.transform.parent = deathTextHandler.transform;
+		}
+		else if(otherTag == "Bullet"){
+			bulletReason.transform.parent = deathTextHandler.transform;
+		}
+		else if(otherTag == "Turret"){
+			turretReason.transform.parent = deathTextHandler.transform;
+		}
+		else{
+			unknownReason.transform.parent = deathTextHandler.transform;
+		}
 	}
 }
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+
+	[SerializeField]
+	private Transform playerTransform;
+	private TextMesh text;
+
+	private float highscore = 0f;
+	void Start(){
+		text = gameObject.GetComponent<TextMesh>();
+	}
+	void Update () {
+		if(playerTransform.position.y > highscore){
+			highscore = playerTransform.position.y;
+		}
+		if(playerTransform.GetComponent<PlayerScript>().isDead){
+			if(PlayerPrefs.GetFloat("PlayerScore") < highscore){
+				PlayerPrefs.SetFloat("PlayerScore",highscore);
+			}
+		}
+		text.text = "Score: " + highscore.ToString ("f1");
+	}
+}
